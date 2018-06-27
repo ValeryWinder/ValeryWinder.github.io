428a1497357152e7650cee83e2dc3a5f2e6928be
ValeryWinder <valera.pukas14@gmail.com>
2018-06-27 15:51:39 +0300

    Smitler Commit

---
+++ Smitler/app/js/common.js
@@ -0,0 +1,35 @@
+$(function() {
+
+	$('#my-menu').mmenu({
+		extensions: [ 'widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black' ],
+		navbar: {
+			title: '<img src="img/logo-1.svg" alt="Салон красоты Смитлер">'
+		},
+		offCanvas: {
+			position: 'right'
+		}
+	});
+
+	var api = $('#my-menu').data('mmenu');
+	api.bind('opened', function() {
+		$('.hamburger').addClass('is-active');
+	}).bind('closed', function() {
+		$('.hamburger').removeClass('is-active');
+	});
+
+	$('.carousel-services').owlCarousel({
+		loop: true,
+		nav: true,
+		smartSpeed: 700,
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>']
+	});
+
+	function carouselService() {
+		$('.carousel-services-item').each(function() {
+			var ths = $(this);
+			var thsh = ths.find('.carousel-services-content').outerHeight();
+			ths.find('.carousel-services-image').css('min-height', thsh);
+		});
+	}carouselService();
+
+});
